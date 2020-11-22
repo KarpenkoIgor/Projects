48bc186c6570000b34dbf24534a68e6249621b46
KarpenkoIgor <62311623+KarpenkoIgor@users.noreply.github.com>
2020-11-22 23:33:57 +0300

    f

---
+++ Labs_ISP/Lab_2/Archiver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace WindowsService1
+{
+    class Archiver
+    {
+        public static void Compress(string sourceFile, string compressedFile)
+        {
+
+            try
+            {
+
+                using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+                {
+                    using (FileStream targetStream = File.Create(compressedFile))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                        {
+                            sourceStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter writer = new StreamWriter("D:\\Target\\sourcelog.txt", true))
+                {
+                    writer.WriteLine("Data archiving error:{0}", ex.Message);
+                    writer.Flush();
+                }
+            }
+        }
+
+        public static void Decompress(string compressedFile, string targetFile)
+        {
+            using (FileStream sourceStream = new FileStream(compressedFile, FileMode.OpenOrCreate))
+            {
+                using (FileStream targetStream = File.Create(targetFile))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(targetStream);
+                    }
+                }
+            }
+        }
+    }
+}
