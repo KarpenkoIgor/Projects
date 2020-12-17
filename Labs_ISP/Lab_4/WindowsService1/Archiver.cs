0903179ff32966c82e5e0cf2419d6411b8a1aef3
KarpenkoIgor <62311623+KarpenkoIgor@users.noreply.github.com>
2020-12-17 17:18:50 +0300

    ws1

---
+++ Labs_ISP/Lab_4/WindowsService1/Archiver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace WindowsService1
+{
+    class Archiver
+    {
+        public static void Compress(string sourceFile, string compressedFile, bool needToEncrypt)
+        {
+            try
+            {
+                string encrFileName = Crypt.EncryptedFilePath(sourceFile, Path.Combine(Path.GetDirectoryName(compressedFile),"crypt"));
+                using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+                {
+                    using (FileStream targetStream = File.Create(compressedFile))
+                    {
+                        using (FileStream encryptStream = File.Create(encrFileName))
+                        {
+                            if(needToEncrypt)
+                                Crypt.Encrypt(sourceStream, encryptStream);
+                            else
+                                sourceStream.CopyTo(encryptStream);
+                            using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                            {
+                                using (FileStream finalStream = new FileStream(encrFileName, FileMode.OpenOrCreate))
+                                {
+                                    finalStream.CopyTo(compressionStream);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sourcelog.txt"), true))
+                {
+                    writer.WriteLine("Ошибка архивации данных:{0}", ex.Message);
+                    writer.Flush();
+                }
+            }
+        }
+
+        public static void Decompress(string compressedFile, string targetFile, bool needToEncrypt)
+        {
+            try
+            {
+                string decrFileName = Crypt.DecryptedFilePath(compressedFile, Path.Combine(Path.GetDirectoryName(targetFile), "crypt"));
+                using (FileStream sourceStream = new FileStream(compressedFile, FileMode.OpenOrCreate))
+                {
+                    using (FileStream targetStream = File.Create(targetFile))
+                    {
+                        using (FileStream decryptStream = File.Create(decrFileName))
+                        {
+                            using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                            {
+                                decompressionStream.CopyTo(decryptStream);
+                            }
+ 
+                        }
+                        using (FileStream finaleStream = new FileStream(decrFileName, FileMode.OpenOrCreate))
+                        {
+
+                            if (needToEncrypt)
+                                Crypt.Decrypt(finaleStream, targetStream);
+                            else
+                                finaleStream.CopyTo(targetStream);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter writer = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sourcelog.txt"), true))
+                {
+                    writer.WriteLine("Ошибка деархивации данных:{0}", ex.Message);
+                    writer.Flush();
+                }
+            }
+
+        }
+    }
+
+   
+}
---
+++ Labs_ISP/Lab_4/WindowsService1/Logger.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using ConfigurationManager;
+
+namespace WindowsService1
+{
+    class Logger
+    {
+      
+            FileSystemWatcher watcher;
+            object obj = new object();
+            bool enabled = true;
+            Settings settings;
+            string sourcePath;
+            string targetPath;
+            bool needToArchive;
+            bool needToEncrypt;
+
+
+            public Logger()
+            {      
+                ConfigurationSettings confSettings = new ConfigurationSettings(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.xml"));          
+                settings = confSettings.GetSetting<Settings>();             
+                if (settings.SourcePath != null) this.sourcePath = settings.SourcePath;
+                if (settings.TargetPath != null) this.targetPath = settings.TargetPath;
+                this.needToArchive = settings.ArchiveSetting.NeedToArchive;
+                this.needToEncrypt = settings.ArchiveSetting.NeedToEncrypt;
+        
+            watcher = new FileSystemWatcher(sourcePath);
+                watcher.Deleted += Watcher_Deleted;
+                watcher.Created += Watcher_Created;
+                watcher.Changed += Watcher_Changed;
+                watcher.Renamed += Watcher_Renamed;
+            }
+
+            public void Start()
+            {
+                watcher.EnableRaisingEvents = true;
+                while (enabled)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+            public void Stop()
+            {
+                watcher.EnableRaisingEvents = false;
+                enabled = false;
+            }
+  
+            private void Watcher_Renamed(object sender, RenamedEventArgs e)
+            {
+                string fileEvent = "переименован в " + e.FullPath;
+                string filePath = e.OldFullPath;
+                RecordEntry(fileEvent, filePath);
+                string gzfile = Path.ChangeExtension(e.Name, "gz");
+                string tocomp = Path.Combine(targetPath, "archive", gzfile);
+                if (needToArchive)
+                {  
+                    Archiver.Compress(e.FullPath, tocomp, needToEncrypt);
+                    Archiver.Decompress(tocomp, Path.ChangeExtension(tocomp, "txt"), needToEncrypt);
+                }
+            }
+
+            private void Watcher_Changed(object sender, FileSystemEventArgs e)
+            {
+                string fileEvent = "изменен";
+                string filePath = e.FullPath;
+                RecordEntry(fileEvent, filePath);
+                string gzfile = Path.ChangeExtension(e.Name, "gz");
+                string tocomp = Path.Combine(targetPath, "archive", gzfile);
+                if (needToArchive)
+                {
+                    Archiver.Compress(e.FullPath, tocomp, needToEncrypt);
+                    Archiver.Decompress(tocomp, Path.ChangeExtension(tocomp, "txt"), needToEncrypt);
+                }
+            }
+            private void Watcher_Created(object sender, FileSystemEventArgs e)
+            {
+                string fileEvent = "создан";
+                string filePath = e.FullPath;
+                RecordEntry(fileEvent, filePath);
+                string gzfile = Path.ChangeExtension(e.Name, "gz");
+                string tocomp = Path.Combine(targetPath,"archive", gzfile);
+                if (needToArchive)
+                {
+                    Archiver.Compress(e.FullPath, tocomp, needToEncrypt);
+                    Archiver.Decompress(tocomp, Path.ChangeExtension(tocomp, "txt"), needToEncrypt);
+                }
+            }
+
+            private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+            {
+                string fileEvent = "удален";
+                string filePath = e.FullPath;
+                RecordEntry(fileEvent, filePath);
+            }
+
+            private void RecordEntry(string fileEvent, string filePath)
+            {
+                lock (obj)
+                {
+                    using (StreamWriter writer = new StreamWriter(Path.Combine(targetPath, "templog.txt"), true))
+                    {
+                        writer.WriteLine(String.Format("{0} файл {1} был {2}",
+                            DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                        writer.Flush();
+                    }
+                }
+            }
+           public string DataPath()
+           {
+                 return DateTime.Now.ToString("yyyy\\MM\\dd");
+           }
+    }
+}
